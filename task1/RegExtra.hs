fe26634651762c6fda54712aa73bdfb7fc1faeae
Peter Rybicki <avelov.mail@gmail.com>
2018-03-24 00:32:18 +0100

    End of day

--- task1/RegExtra.hs
+++ task1/RegExtra.hs
@@ -10,29 +10,48 @@ class Equiv a where
   (===) :: a -> a -> Bool
 
 instance (Eq c) => Equiv (Reg c) where
-  Eps === Eps = True
-  Empty === Empty = True
-  Lit a === Lit b = True
-  Many l === Many r = l === r
-  l1 :| l2 === r1 :| r2 = (l1 === r1 && l2 === r2) || (l1 === r2 && l2 === r1)
-  -- l1 :> l2 === r1 :> r2 =
-  l === r = l == r
+  l === r = simpl l == simpl r
+  -- l1 :| l2 === r1 :| r2 = let (l1' :| l2') = simpl (l1 :| l2)
+  --                             (r1' :| r2') = simpl (r1 :| r2)
+  --                         in  (l1' === r1' && l2' === r2')
+  --                         ||  (l1' === r2' && l2' === r1')
+  -- l1 :| l2 === r = simpl (l1 :| l2) == simpl r
+  -- l === r1 :| r2 = r1 :| r2 === l
+  -- l1 :> l2 === r1 :> r2 = let (l1' :| l2') = simpl (l1 :| l2)
+  --                             (r1' :| r2') = simpl (r1 :| r2)
+  --                         in  l1' === r1' && l2' === r2'
 
 instance Mon (Reg c) where
   m1 = Eps
-  x <> y = x :| y
+  x <> y = x :> y
 
 simpl :: Reg c -> Reg c
-simpl (l :| Empty) = simpl l
-simpl (Empty :| r) = simpl r
-simpl (x :| Eps) = if nullable x then simpl x else (simpl x :| simpl Eps)
-simpl (Eps :| x) = simpl (x :| Eps)
-simpl (l :| r) = (simpl l :| simpl r)
-simpl (l :> Empty) = simpl l
-simpl (Empty :> r) = simpl r
-simpl (l :> Eps) = simpl l
-simpl (Eps :> r) = simpl r
-simpl (l :> r) = (simpl l :> simpl r)
+-- simpl (Empty :> _) = Empty
+-- simpl (Empty :| x) = x
+-- simpl (Eps :> x) = x
+-- simpl (Many x) = Many (simpl x)
+simpl (l :> r) = simpl $ foldl (:>) Eps (linerizeConcat (l :> r)) where
+    linerizeConcat (l :> r) = (linerizeConcat (simpl l)) ++ (linerizeConcat (simpl r))
+    linerizeConcat Eps = []
+    linerizeConcat x = [x]
+simpl (l :| r) = simpl $ foldl (:|) Empty (linearizeSum (l :| r)) where
+  linearizeSum (l :| r) = (linearizeSum (simpl l)) ++ (linearizeSum (simpl r))
+  linearizeSum Empty = []
+  linearizeSum x = [x]
+simpl x = x
+
+-- simpl (l :| Empty) = Empty
+-- simpl (Empty :| r) = Empty
+-- simpl (x :| Eps) = if nullable x then simpl x else (simpl x :| simpl Eps)
+-- simpl (Eps :| x) = simpl (x :| Eps)
+-- simpl (l :| r) = (simpl l :| simpl r)
+--
+-- simpl (l :> Empty) = simpl l
+-- simpl (Empty :> r) = simpl r
+-- simpl (l :> Eps) = simpl l
+-- simpl (Eps :> r) = simpl r
+-- simpl (l :> r) = (simpl l :> simpl r)
+
 simpl (Many r) = Many (simpl r)
 simpl r = r   -- Lit, Eps, Empty
 
